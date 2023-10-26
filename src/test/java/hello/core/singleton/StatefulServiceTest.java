5ea113f86e31df0953d0366dff4570b8bd76b758
admin <gkaeogus7@gmail.com>
2023-10-26 17:44:28 +0900

    20231026

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,21 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public
+    }
+
+}
